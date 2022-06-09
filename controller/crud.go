a07e9a0503f034ef1a49a24f46ffc013eeaeffb9
mehulsoni_bcs <mehul.soni@bcssarl.com>
2022-06-09 14:55:27 +0530

    Select Data by Querying field

--- controller/crud.go
+++ controller/crud.go
@@ -19,7 +19,9 @@ func todo() http.HandlerFunc {
 			w.WriteHeader(http.StatusCreated)
 			json.NewEncoder(w).Encode(data)
 		} else if r.Method == http.MethodGet {
-			data, err := model.ReadAll()
+			// data, err := model.ReadAll()
+			name := r.URL.Query().Get("name")
+			data, err := model.ReadByName(name)
 			if err != nil {
 				w.Write([]byte(err.Error()))
 			}
--- model/todo.go
+++ model/todo.go
@@ -33,3 +33,20 @@ func ReadAll() ([]view.PostRequest, error) {
 
 	return todos, err
 }
+
+func ReadByName(name string) ([]view.PostRequest, error) {
+	rows, err := con.Query("SELECT * FROM ToDo WHERE name = ?", name)
+	if err != nil {
+		return nil, err
+	}
+
+	todos := []view.PostRequest{}
+
+	for rows.Next() {
+		data := view.PostRequest{}
+		rows.Scan(&data.Name, &data.Todo)
+		todos = append(todos, data)
+	}
+
+	return todos, err
+}
